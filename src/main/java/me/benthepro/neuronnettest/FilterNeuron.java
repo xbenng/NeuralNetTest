6ee25bafeba32a849a5479b6f4b0891a59d4db5f
Ben Ng <xbenng@gmail.com>
2015-07-22 02:34:08 -0400

    bad

--- src/main/java/me/benthepro/neuronnettest/FilterNeuron.java
+++ src/main/java/me/benthepro/neuronnettest/FilterNeuron.java
@@ -4,4 +4,43 @@ package me.benthepro.neuronnettest;
  * Created by ben on 7/21/15.
  */
 public class FilterNeuron {
+    private int filterSizeX;
+    private int filterSizeY;
+    private int depth;
+    private Neuron[][][] inputNeurons;
+    private Neuron outputNeuron = new Neuron(Neuron.HIDDEN);
+
+
+    FilterNeuron(int x, int y, int depth)
+    {
+        this.filterSizeX = x;
+        this.filterSizeY = y;
+        this.depth = depth;
+        inputNeurons = new Neuron[filterSizeX][filterSizeY][depth];  //convolutional layer 1, nxnxrgb
+        for (x = 0; x < filterSizeX; x++) {
+            for (y = 0; y < filterSizeY; y++) {
+                for (int d = 0; d < d; d++) {
+                    inputNeurons[x][y][d] = new Neuron(Neuron.INPUT);
+                    outputNeuron.addInputNeuron(inputNeurons[x][y][d], Utils.rand(-1, 1));
+                }
+            }
+        }
+    }
+
+    public void setInput(float[][][] input)
+    {
+        for (int x = 0; x < filterSizeX; x++) {
+            for (int y = 0; y < filterSizeY; y++) {
+                for (int d = 0; d < d; d++) {
+                    inputNeurons[x][y][d].setInput(input[x][y][d]);
+                }
+            }
+        }
+    }
+
+
+    public Neuron getOutputNeuron()
+    {
+        return outputNeuron;
+    }
 }
--- src/main/java/me/benthepro/neuronnettest/Utils.java
+++ src/main/java/me/benthepro/neuronnettest/Utils.java
@@ -1,7 +1,16 @@
 package me.benthepro.neuronnettest;
 
+import java.util.Random;
+
 /**
  * Created by ben on 7/21/15.
  */
 public class Utils {
+    private static Random rand = new Random(NeuronNet.startTime);
+
+    public static float rand(int a, int b)
+    {
+        return rand.nextFloat() * (b - a) - b;
+    }
+
 }
